2d7e9f636dac65c4cf70f8c713104a5b8884a199
MehdiBelmahboubi <belmehboubim@gmail.com>
2024-05-02 19:56:33 +0100

    Initializing Admin,Employes,RH interfaces to front end.

--- frontend-Angular/src/app/app-routing.module.ts
+++ frontend-Angular/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeTemplateComponent} from "./home/home-template.component";
-import {LoginComponent} from "./login/login.component";
-import {CandidatureComponent} from "./candidature/candidature.component";
+import {HomeTemplateComponent} from "./User-Interface/home/home-template.component";
+import {LoginComponent} from "./User-Interface/login/login.component";
+import {CandidatureComponent} from "./User-Interface/candidature/candidature.component";
+import {DepartementComponent} from "./Admin-Interface/departement/departement.component";
+import {EmployesComponent} from "./Admin-Interface/employes/employes.component";
+import {RhComponent} from "./Admin-Interface/rh/rh.component";
 
 
 const routes: Routes = [
@@ -10,6 +13,9 @@ const routes: Routes = [
   {path:"home",component:HomeTemplateComponent},
   {path:"login",component:LoginComponent},
   {path:"candidature",component:CandidatureComponent},
+  {path:"departement",component:DepartementComponent},
+  {path:"employes",component:EmployesComponent},
+  {path:"rh",component:RhComponent},
 ];
 
 @NgModule({
--- frontend-Angular/src/app/app.module.ts
+++ frontend-Angular/src/app/app.module.ts
@@ -6,10 +6,21 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
 import {MatButton, MatButtonModule} from "@angular/material/button";
-import { HomeTemplateComponent } from './home/home-template.component';
-import { LoginComponent } from './login/login.component';
-import { CandidatureComponent } from './candidature/candidature.component';
-import { UserComponent } from './user/user.component';
+import { HomeTemplateComponent } from './User-Interface/home/home-template.component';
+import { LoginComponent } from './User-Interface/login/login.component';
+import { CandidatureComponent } from './User-Interface/candidature/candidature.component';
+import { UserComponent } from './User-Interface/user/user.component';
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatOption, MatSelect, MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+import { AdminComponent } from './Admin-Interface/admin/admin.component';
+import { EmployesComponent } from './Admin-Interface/employes/employes.component';
+import { RhComponent } from './Admin-Interface/rh/rh.component';
+import { DepartementComponent } from './Admin-Interface/departement/departement.component';
+import { SettingComponent } from './Admin-Interface/setting/setting.component';
+import {MatDrawer, MatDrawerContainer, MatDrawerContent} from "@angular/material/sidenav";
+import {MatList, MatListItem, MatListModule} from "@angular/material/list";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
@@ -18,13 +29,24 @@ import { UserComponent } from './user/user.component';
     HomeTemplateComponent,
     LoginComponent,
     CandidatureComponent,
-    UserComponent
+    UserComponent,
+    AdminComponent,
+    EmployesComponent,
+    RhComponent,
+    DepartementComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
     MatButtonModule,
+    MatDrawerContainer,
+    MatDrawer,
+    MatListModule,
+    MatListItem,
+    MatDrawerContent,
+    MatIconModule
   ],
   providers: [
     provideAnimationsAsync()
