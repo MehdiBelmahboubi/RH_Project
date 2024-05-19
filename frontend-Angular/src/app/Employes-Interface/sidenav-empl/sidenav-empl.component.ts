9c07e2e85f512a24615eea36151b46d3454d1b65
hanae <hanaebninou28@gmail.com>
2024-05-19 01:02:32 +0100

    interface empl 2

--- frontend-Angular/src/app/Employes-Interface/sidenav-empl/sidenav-empl.component.ts
+++ frontend-Angular/src/app/Employes-Interface/sidenav-empl/sidenav-empl.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav-empl',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './sidenav-empl.component.css'
 })
 export class SidenavEmplComponent {
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  nom: string | null = null;
+  prenom: string | null = null;
+  fonction: string | null = null;
+
+  constructor(private observer: BreakpointObserver, private router: Router) {}
+
+  ngAfterViewInit() {
+    this.nom = localStorage.getItem('nom');
+    this.prenom = localStorage.getItem('prenom');
+    this.fonction = localStorage.getItem('fonction');
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+  }
 
 }
