9fb8274770b4e44178dc4513e2dd7e2dbd0e4cb4
hanae <hanaebninou28@gmail.com>
2024-05-24 00:34:39 +0100

    setings admin/rh/empl

--- frontend-Angular/src/app/Rh-Interface/profile-rh/profile-rh.component.ts
+++ frontend-Angular/src/app/Rh-Interface/profile-rh/profile-rh.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile-rh',
   templateUrl: './profile-rh.component.html',
-  styleUrl: './profile-rh.component.css'
+  styleUrls: ['./profile-rh.component.css']
 })
 export class ProfileRhComponent {
 
+  rh = {
+    cin: '12345678',
+    nom: 'lola',
+    prenom: 'lme7loula',
+    dateNaissance: '2002-01-01',
+    telephone: '1234567890',
+    email: 'lolalme7loula@mail.com',
+    contrat: 'CDI',
+    fonction: 'chef',
+    salaire: '7000',
+    cnss: '11223344',
+    dateRecrutement: '2024-01-01'
+  };
+
+
+  newEmail: string = '';
+  oldPassword: string = '';
+  newPassword: string = '';
+
+
+  saveProfile() {
+
+  }
+
+
+  changePassword() {
+
+  }
+
+  changeEmail() {
+
+  }
 }
